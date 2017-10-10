5b0e15b347696fce6333a2cb07fc6b7bc20f9b4d
Martin Schoen <schoenm@cip.ifi.lmu.de>
2017-10-10 23:36:45 +0200

    Add /dudensearch route for parsing and returning duden search results. Also use request module instead of http

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,4 +1,6 @@
-const http = require('http');
+const request = require('request');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 module.exports = function (app) {
 
@@ -10,32 +12,53 @@ module.exports = function (app) {
    */
   app.get('/dudendirect/:query', (req, res) => {
     const query = req.params.query;
-    res.setHeader('Content-Type', 'application/json');
-    http.get('http://www.duden.de/search_duden/autocomplete/'+query, httpResponse => {
-      var receivedData = '';
-      httpResponse.on('data', chunk => {
-        receivedData += chunk;
-      });
-      httpResponse.on('end', () => {
-        var parsedData = (receivedData !== '') ? JSON.parse(receivedData) : {};
-        var lexeme = parsedData.lexeme ? parsedData.lexeme.result : null;
-        if (lexeme) {
-          var filteredLexeme = lexeme.filter(entry => {
-            if (entry.ignore)
-              return false;
-            return true;
-          });
-          var exposeUrls = filteredLexeme.map(entry => {
-            return {
-              'key': entry.key,
-              'url': entry.label.split('href=\"')[1].split('\">')[0]
-            };
-          });
-          res.send(JSON.stringify(exposeUrls));
-        } else {
-          res.send('[]');
+    request('http://www.duden.de/search_duden/autocomplete/' + query, (err, httpResponse, body) => {
+      if (err) throw err;
+      var parsedData = (body !== '') ? JSON.parse(body) : {};
+      var lexeme = parsedData.lexeme ? parsedData.lexeme.result : null;
+      if (lexeme) {
+        var filteredLexeme = lexeme.filter(entry => {
+          if (entry.ignore)
+            return false;
+          return true;
+        });
+        var exposeUrls = filteredLexeme.map(entry => {
+          return {
+            'key': entry.key,
+            'url': entry.label.split('href=\"')[1].split('\">')[0]
+          };
+        });
+        res.send(JSON.stringify(exposeUrls));
+      } else {
+        res.send('[]');
+      }
+    });
+  });
+
+  /**
+   * Returns all search results that are displayed on the first page
+   * of a search request on duden.de
+   *
+   * @param query: the string to search for
+   */
+  app.get('/dudensearch/:query', (req, res) => {
+    const query = req.params.query;
+    request('http://www.duden.de/suchen/dudenonline/' + query, (err, httpResponse, body) => {
+      const dom = new JSDOM(body);
+      const document = dom.window.document;
+
+      const entries = document.querySelectorAll('section.wide h2 a');
+
+      var result = [];
+
+      entries.forEach(entry => {
+        result[result.length] = {
+          key: entry.textContent,
+          url: entry.getAttribute('href')
         }
       });
+
+      res.send(result);
     });
   });
 };
